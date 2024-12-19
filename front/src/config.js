9b4ea81765197ab9ebaa24dfddd4a01c3ef27fef
AlfaPatens <aleksasdar@gmail.com>
2024-12-19 15:17:25 +0200

    update url

--- front/src/config.js
+++ front/src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = "https://localhost:7294/api/v1"; // Update with your backend URL if hosted elsewhere
+const API_BASE_URL = "https://octopus-app-a3x4s.ondigitalocean.app/api/v1"; // Update with your backend URL if hosted elsewhere
 export default API_BASE_URL;
